285ab777affc62b5f16fc93cd61fd6b5adb79a19
Vitaly Yerofeyevsky <flytaly@gmail.com>
2020-05-12 00:00:16 +0300

    Display wind

--- src/sketch.ts
+++ src/sketch.ts
@@ -9,6 +9,7 @@ let dragC = 0.2;
 let frictionMag = 0.1;
 let lines: Line[] = [];
 let aquarium: Aquarium;
+let windLines = [];
 
 const sketch = (p: p5) => {
   p.setup = () => {
@@ -26,6 +27,14 @@ const sketch = (p: p5) => {
 
     balls.forEach((obj) => obj.show());
     aquarium = new Aquarium(p, p.width / 2, p.height / 3);
+    for (let i = 0; i < 40; i++) {
+      const obj = {
+        x: Math.random() * p.width,
+        y: Math.random() * p.height,
+        width: Math.random() * 4,
+      };
+      windLines.push(obj);
+    }
   };
 
   p.draw = () => {
@@ -46,6 +55,12 @@ const sketch = (p: p5) => {
         if (p.mouseIsPressed) {
           let wind = p.createVector(0.9, 0);
           ball.applyForce(wind);
+          p.stroke('grey');
+          p.strokeWeight(1);
+          windLines.forEach((element) => {
+            element.x = (element.x + wind.x) % p.width;
+            p.line(element.x, element.y, element.x + element.width, element.y);
+          });
         }
       }
       lines.forEach((line) => line.bounce(ball));
